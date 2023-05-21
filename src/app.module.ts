db541adf1b4d6bd5d599aa5a139a213b96e603b7
Renan Luis <RenanLuis2001@outlook.com>
2023-05-21 18:31:51 -0300

    created base files : prisma, task, user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TaskModule, PrismaModule],
   controllers: [],
   providers: [],
 })
